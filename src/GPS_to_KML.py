b95c47be842c98271e15e34e6ca8d4ca591fc422
David <dbm2186@rit.edu>
2021-10-12 21:25:48 -0400

    Adding in initial CheckPoint 1 Code - Reads in, converts RMC to KML - Generates KML - Ignores some anomalies, not many

--- src/GPS_to_KML.py
+++ src/GPS_to_KML.py
@@ -1,3 +1,43 @@
+def emit_header(fp_out):
+    fp_out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+    fp_out.write("<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n")
+    fp_out.write("  <Document>\n")
+    fp_out.write("    <name>Paths</name>\n")
+    fp_out.write("    <description>Examples of paths. Note that the tessellate tag is by default\n")
+    fp_out.write("      set to 0. If you want to create tessellated lines, they must be authored\n")
+    fp_out.write("      (or edited) directly in KML.</description>\n")
+    fp_out.write("    <Style id=\"yellowLineGreenPoly\">\n")
+    fp_out.write("      <LineStyle>\n")
+    fp_out.write("        <color>7f00ffff</color>\n")
+    fp_out.write("        <width>4</width>\n")
+    fp_out.write("      </LineStyle>\n")
+    fp_out.write("      <PolyStyle>\n")
+    fp_out.write("        <color>7f00ff00</color>\n")
+    fp_out.write("      </PolyStyle>\n")
+    fp_out.write("    </Style>\n")
+    fp_out.write("    <Placemark>\n")
+    fp_out.write("      <name>Absolute Extruded</name>\n")
+    fp_out.write("      <description>Transparent green wall with yellow outlines</description>\n")
+    fp_out.write("      <styleUrl>#yellowLineGreenPoly</styleUrl>\n")
+    fp_out.write("      <LineString>\n")
+    fp_out.write("        <extrude>1</extrude>\n")
+    fp_out.write("        <tessellate>1</tessellate>\n")
+    fp_out.write("        <altitudeMode>absolute</altitudeMode>\n")
+    fp_out.write("        <coordinates>  ")
+
+def emit_coordinates(fp_out, lines):
+    for item in lines:
+        fp_out.write(str(item[0]) + "," + str(item[1]) + "," + str(item[2]) + "\n")
+
+
+def emit_trailer(fp_out):
+    fp_out.write("        </coordinates>\n")
+    fp_out.write("      </LineString>\n")
+    fp_out.write("    </Placemark>\n")
+    fp_out.write("  </Document>\n")
+    fp_out.write("</kml>\n")
+
+
 def read_and_parse(filename):
     file = open(filename, "r")
 
@@ -11,16 +51,56 @@ def read_and_parse(filename):
     stripped_list = []
     for value in contents_list:
         stripped_list.append(value.strip())
-    two_points = [stripped_list[0], stripped_list[1]]
-    return two_points
+    return stripped_list
 
+def clean_lines(lines):
+    clean = []
+    for value in lines:
+        temp = value.split(",")
+        RMC = value.count("GPRMC")
+        GGA = value.count("GPGGA")
+        if RMC == 1 and GGA == 0:
+            clean.append((temp[3], temp[4], temp[5], temp[6], temp[7]))
+    del lines
+    return clean
+
+
+def calculate_lat_long(clean):
+    final = []
+    for value in clean:
+        hold_num = float(value[0]) // 100
+        degrees = float(value[0]) % 100
+        degrees = degrees / 60
+
+        if(value[1] == "N"):
+            long = str(hold_num + degrees)
+        else:
+            long = "-" + str(hold_num + degrees)
+
+        hold_num = float(value[2]) // 100
+        degrees = float(value[2]) % 100
+        degrees = degrees / 60
+
+        if(value[3] == "E"):
+            lat = str(hold_num + degrees)
+        else:
+            lat = "-" + str(hold_num + degrees)
+
+        final.append((lat, long, value[4]))
+
+    del clean
+    return final
 
 def main():
-    FILENAME = "2021_09_AA__GPS_CHECK.txt"
-    two_points = read_and_parse(FILENAME)
-    print(two_points)
-    return None
+    raw_lines = read_and_parse("2021_09_08__181321_gps_JERAMIAS_and_BACK.txt")
+    clean = clean_lines(raw_lines)
+    final = calculate_lat_long(clean)
 
+    fp_out = open("Test_Output.kml", "w")
 
-if __name__ == "__main__":
+    emit_header(fp_out)
+    emit_coordinates(fp_out, final)
+    emit_trailer(fp_out)
+
+if __name__ == "__main__" :
     main()
