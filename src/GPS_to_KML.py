b36b0cc39ebde593ebe94ff26782fc92e5dce07e
David <dbm2186@rit.edu>
2021-11-03 20:31:39 -0400

    - Merged in reading of multiple files - Added 'stops', if the stop is more than 2 seconds

--- src/GPS_to_KML.py
+++ src/GPS_to_KML.py
@@ -7,12 +7,13 @@ COLOR_MAGENTA = "ff00ff"
 
 
 class Point:
-    def __init__(self, latitude, longitude, speed, angle, color):
+    def __init__(self, latitude, longitude, speed, angle, color, time):
         self.latitude = latitude
         self.longitude = longitude
         self.speed = speed
         self.angle = angle
         self.color = color
+        self.time = time
 
     def change_color(self, color):
         self.color = color
@@ -81,6 +82,24 @@ def emit_placemarker_end(fp_out):
     fp_out.write("    </Placemark>\n")
 
 
+def emit_stop_sign(fp_out, item):
+
+    fp_out.write("    <Placemark>\n")
+    fp_out.write("        <description>Red PIN for A Stop</description>\n")
+    fp_out.write("        <Style id=\"normalPlacemark\">\n")
+    fp_out.write("            <IconStyle>\n")
+    fp_out.write("                <color>ff0000ff</color>\n")
+    fp_out.write("                    <Icon>\n")
+    fp_out.write("                        <href>http://maps.google.com/mapfiles/kml/paddle/1.png</href>\n")
+    fp_out.write("                    </Icon>\n")
+    fp_out.write("            </IconStyle>\n")
+    fp_out.write("        </Style>\n")
+    fp_out.write("        <Point>\n")
+    fp_out.write("            <coordinates>" + str(item.latitude) + "," + str(item.longitude) + "," + str(item.speed) + "</coordinates>\n")
+    fp_out.write("        </Point>\n")
+    fp_out.write("    </Placemark>\n")
+
+
 def emit_trailer(fp_out):
     fp_out.write("  </Document>\n")
     fp_out.write("</kml>\n")
@@ -109,7 +128,7 @@ def clean_lines(lines):
         RMC = value.count("GPRMC")
         GGA = value.count("GPGGA")
         if RMC == 1 and GGA == 0:
-            clean.append((temp[3], temp[4], temp[5], temp[6], temp[7], temp[8]))
+            clean.append((temp[3], temp[4], temp[5], temp[6], temp[7], temp[8], temp[1]))
     del lines
     return clean
 
@@ -135,7 +154,7 @@ def calculate_lat_long(clean):
         else:
             lat = "-" + str(hold_num + degrees)
 
-        final.append(Point(lat, long, value[4], value[5], COLOR_MAGENTA))
+        final.append(Point(lat, long, value[4], value[5], COLOR_MAGENTA, value[6]))
 
     del clean
     return final
@@ -145,7 +164,6 @@ def get_distance(pos_1, pos_2):
     lat_diff = abs(float(pos_1.latitude) - float(pos_2.latitude)) * 364000
     long_diff = abs(float(pos_1.longitude) - float(pos_2.longitude)) * 288200
     return long_diff + lat_diff
-    #return sqrt((lat_diff * lat_diff) + (long_diff + long_diff))
 
 
 def get_degrees(pos_1, pos_2):
@@ -156,6 +174,22 @@ def get_degrees(pos_1, pos_2):
         return abs(temp), "right"
 
 
+
+def get_time_diff(prev_point, second_point):
+    return abs(float(second_point.time) - float(prev_point.time))
+
+
+def try_to_append(stops, second_point):
+    temp = True
+    for point in stops:
+        dist = get_distance(point, second_point)
+        if dist < 1000:
+            temp = False
+            break
+    if temp:
+        stops.append(second_point)
+
+
 def main(argv):
 
     # Collect the argCount for argument checking
@@ -164,6 +198,9 @@ def main(argv):
     arguments = []
 
     # Check for AT LEAST one text file
+    if len(argv) < 3:
+        print("You must enter at least one text file and one kml file output name to run this program.")
+        exit(0)
     if argv[1][argv[1].index("."):] != ".txt":
         print("You must enter at least one text file to run this program.")
         exit(0)
@@ -171,7 +208,6 @@ def main(argv):
     # Given we have a single text file, check the rest of the arguments.
     for arg in argv:
         extension = arg[arg.index("."):]
-        print(extension)
         # If we are on index 0, the value is the python file. Skip it.
         if argIdx > 0:
             if argIdx == argCount - 1:
@@ -199,8 +235,9 @@ def main(argv):
             list_of_lat_longs.append(calculate_lat_long(clean))
             argIdx += 1
 
+    nf = []
+    stops = []
     for final in list_of_lat_longs:
-        nf = []
         not_turn = []
         idx = 0
         while idx < len(final):
@@ -210,7 +247,7 @@ def main(argv):
                 pos2 = final[idx2]
                 if get_distance(pos1, pos2) > 200:
                     result = get_degrees(pos1, pos2)
-                    if result[0] > 65:
+                    if result[0] > 65 and ((abs(float(pos1.latitude) - float(pos2.latitude)) * 364000) > 50 and (abs(float(pos1.longitude) - float(pos2.longitude)) * 288200 > 50)):
                         temp_array = final[idx-1:idx2+1]
                         nf.append((not_turn, "not"))
                         nf.append((temp_array, result[1]))
@@ -228,7 +265,23 @@ def main(argv):
         if not_turn:
             nf.append((not_turn, "not"))
 
-    fp_out = open("Test_Output.kml", "w")
+        idx = 0
+        prev_point = final[0]
+        second_point = final[0]
+        while idx < len(final):
+            current_point = final[idx]
+            if current_point.latitude == prev_point.latitude and current_point.longitude == prev_point.longitude:
+                second_point = current_point
+            else:
+                # check time between second point and prev point
+                time_diff = get_time_diff(prev_point, second_point)
+                if time_diff >= 2:
+                    try_to_append(stops, second_point)
+                prev_point = current_point
+                second_point = current_point
+            idx += 1
+
+    fp_out = open(arguments[-1], "w")
 
     emit_header(fp_out)
     emit_styles(fp_out)
@@ -247,57 +300,10 @@ def main(argv):
             emit_coordinates(fp_out, item[0])
             emit_placemarker_end(fp_out)
 
+    for item in stops:
+        emit_stop_sign(fp_out, item)
+
     emit_trailer(fp_out)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-    new_final = []
-    non_turn = []
-    idx = 0
-    while idx < len(final):
-        idx2 = idx
-        possible_turn = []
-        while idx2 < len(final):
-            pos_1 = final[idx]
-            pos_2 = final[idx2]
-            if get_distance(pos_1, pos_2) > 50:
-                if get_degrees(pos_1, pos_2) > 70:
-                    for i in range (idx, idx2):
-                        possible_turn.append(final[idx])
-                    new_final.append((non_turn, False))
-                    new_final.append((possible_turn, True))
-                    non_turn = []
-                    possible_turn = []
-                    idx = idx2 + 1
-                    idx2 = idx
-                else:
-                    non_turn.append(final[idx])
-                    idx += 1
-            else:
-                non_turn.append((final[idx]))
-            idx2+=1
-        idx+=1
-    new_final.append((non_turn, False))
-"""
-
 
 if __name__ == "__main__":
     main(sys.argv)
